0ee6ef9c49264ac7439801b0cb1eb3a4fc9141bb
Eugene Shapovalov <shapovalovei@gmail.com>
2026-09-01 19:06:04 +0300

    fix: use active runtime dimensions in Mosaic
    Refs #65

--- yolox/data/datasets/mosaicdetection.py
+++ yolox/data/datasets/mosaicdetection.py
@@ -77,7 +77,7 @@ class MosaicDetection(Dataset):
     def __getitem__(self, idx):
         if self.enable_mosaic:
             mosaic_labels = []
-            input_dim = self._dataset.input_dim
+            input_dim = self.input_dim
             input_h, input_w = input_dim[0], input_dim[1]
 
             # yc, xc = s, s  # mosaic center x, y
